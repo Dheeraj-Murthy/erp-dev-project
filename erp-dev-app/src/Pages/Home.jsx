efbec9cdd794ac64fa0479d1f40a0471d7581969
meenigam <ms.dheerajmurthy@iiitb.ac.in>
2024-08-06 21:33:31 +0530

    Added tailwind support and functionality

---
+++ erp-dev-app/src/Pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Login from '../Components/Login'
+
+const Home = () => {
+  return (
+    <div className='text-black'>Home
+      <Login />
+    </div>
+  )
+}
+
+export default Home
